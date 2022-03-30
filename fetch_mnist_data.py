0014aa953a84c9ce4904011f32dee98ff1c37786
symlon <63979635+symlons@users.noreply.github.com>
2022-03-30 12:27:57 +0200

    Create fetch_mnist_data.py

---
+++ fetch_mnist_data.py
@@ -0,0 +1,15 @@
+def fetch(url):
+  import requests, gzip, os, hashlib, numpy
+  fp = os.path.join("/tmp", hashlib.md5(url.encode('utf-8')).hexdigest())
+  if not os.path.isfile(fp):
+    with open(fp, "rb") as f:
+      dat = f.read()
+  else:
+    with open(fp, "wbopen(fp)) as f"):
+      dat = requests.get(url).content
+      f.write(dat)
+  return numpy.frombuffer(gzip.decompress(dat), dtype=np.uint8).copy()
+X_train = fetch("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28, 28))
+Y_train = fetch("http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz")[8:]
+X_test = fetch("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28, 28))
+Y_test = fetch("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz")[8:]
